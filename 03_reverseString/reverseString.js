bd34d83ba998004d93a2b876640563349dd140ca
JollyJoker90 <eike@LAPTOP-22I97KGM.>
2024-11-21 17:12:07 +0100

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let stringArray = String(str).split("");
+    let output = "";
+    for (let i = stringArray.length; i > 0; i--) {
+        output += stringArray[i-1];
+    }
+    return output;
 };
 
 // Do not edit below this line
